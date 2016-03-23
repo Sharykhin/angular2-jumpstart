8e8bf3de1afcd1f96b21e44fc097a579c463466d
chapal <siarhei.sharykhin@gmail.com>
2016-03-24 00:21:09 +0300

    complete service

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -8,4 +8,10 @@ export class HeroService {
 	getHeroes() {
 		return Promise.resolve(HEROES);
 	}
+
+	getHeroesSlowly() {
+	    return new Promise<Hero[]>(resolve =>
+	      setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+	    );
+  }
 }
