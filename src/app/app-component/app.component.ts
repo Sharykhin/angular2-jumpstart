48fefabaed79a371b5c381c5001b5f23d2856bfc
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2016-03-25 07:44:30 +0300

    add gulp less and css minify

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from 'angular2/core';
 @Component({
 	selector: 'app-component',
 	templateUrl: 'app/app-component/app.component.html',
-	styleUrls: ['app/app-component/main.css']
+	styleUrls: ['app/app-component/app.component.css']
 })
 export class AppComponent implements OnInit {
 
