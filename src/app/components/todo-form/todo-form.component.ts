0de633648d7fb13ce8a727699db755c62b79f43b
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2016-04-07 19:02:54 +0300

    little changes

--- src/app/components/todo-form/todo-form.component.ts
+++ src/app/components/todo-form/todo-form.component.ts
@@ -1,19 +1,23 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {TodoListener} from './../../listeners/todo.listener';
 import {Todo} from './../../interfaces/todo.interface';
 import {TodoService} from './../../services/todo.service';
 import {HighlightDirective} from './../../directives/highlight.directive';
+import { Router, RouteParams } from 'angular2/router';
 
 @Component({
 	selector: 'todo-form',
 	templateUrl: '/app/components/todo-form/todo-form.component.html',
 	directives: [HighlightDirective]
 })
-export class TodoFormComponent {
+export class TodoFormComponent implements OnInit {
 
-	constructor(private _todoListener: TodoListener, private _todoService: TodoService) {
-
-	}
+	constructor(
+		private _todoListener: TodoListener,
+		private _todoService: TodoService,
+		private _router: Router,
+		private _routeParams: RouteParams
+		) { }
 
 	onSubmit(title) {
 		this._todoService.createTodo({
@@ -23,5 +27,10 @@ export class TodoFormComponent {
 			todo => this._todoListener.onTodoAdd(todo),
 			err => console.error(err)
 		);
+	}
+
+	ngOnInit() {
+		let id = this._routeParams.get('id');
+		console.log(id);
 	}
 }
