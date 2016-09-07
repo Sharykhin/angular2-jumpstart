8b67210c214243745685bec872eeb9bb64a6f0f3
mac-222 <mac-222@mac-222s-Mac-mini.local>
2016-09-07 17:38:12 +0300

    added http call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UserModule } from './modules/user/user.module';
+import { HttpModule } from '@angular/http';
 
 import { HomeComponent } from './components/home/home.component';
 import { AppComponent }  from './components/app/app.component';
 
-import { routing, appRoutingProviders } from './components/app/app.routing';
+import { routing, appRoutingProviders } from './app.routing';
+
+import { PupilApiService } from './services/pupil-api.service';
 
 
 @NgModule({
-    imports:      [ BrowserModule, routing, UserModule],
+    imports:      [ BrowserModule, routing, UserModule, HttpModule ],
     declarations: [ AppComponent, HomeComponent],
     bootstrap:    [ AppComponent ],
     providers: [
-        appRoutingProviders
-    ],
+        appRoutingProviders,
+        PupilApiService,
+        { provide: 'ApiEndpoint', useValue: 'http://localhost:5000' }
+    ]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import { PupilApiService } from './../../services/pupil-api.service';
 
 @Component({
     selector: 'home',
     templateUrl: 'app/components/home/home.component.html',
     styleUrls: ['app/components/home/home.component.css']
 })
-export class HomeComponent {
-    constructor() {
+export class HomeComponent implements OnInit {
+
+    constructor(
+        private pupilApiService: PupilApiService
+    ) {
         console.log('HomeComponent: constructor');
+    }
+
+    ngOnInit() {
+        console.log('HomeComponent: make api request for getting all the pupils');
+        this.pupilApiService.getPupils().subscribe(pupils => {
+            console.log(pupils);
+        });
     }
 }
