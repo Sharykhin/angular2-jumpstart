9ee770926e1ae03e2be90acccc1dcd95c8b833b3
Sharykhin <siarhei.sharykhin@gmail.com>
2016-09-15 18:07:36 +0300

    some changes

--- src/app/modules/pupil/pupil.module.ts
+++ src/app/modules/pupil/pupil.module.ts
@@ -6,13 +6,14 @@ import { PupilListComponent } from './components/pupil-list/pupil-list.component
 import { PupilProfileComponent } from './components/pupil-profile/pupil-profile.component';
 import { PupilSectionComponent } from './components/pupil-section/pupil-section.component';
 import { CreatePupilComponent } from './components/create-pupil/create-pupil.component';
+import { PupilFormComponent } from './components/pupil-form/pupil-form.component';
 
 import { PupilApiService } from './services/pupil-api.service';
 import { routing } from './pupil.routing';
 
 @NgModule({
     imports:      [ routing, CommonModule ], 
-    declarations: [ PupilComponent, PupilSectionComponent, PupilListComponent, PupilProfileComponent, CreatePupilComponent ],
+    declarations: [ PupilComponent, PupilSectionComponent, PupilListComponent, PupilProfileComponent, CreatePupilComponent, PupilFormComponent ],
     providers:    [
         { provide: 'PupilApiInterface', useClass: PupilApiService }
     ],
