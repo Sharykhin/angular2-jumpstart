e54b6f28765e7e818c74d88f5173b7407b22b526
Sharykhin <siarhei.sharykhin@gmail.com>
2016-09-19 18:35:29 +0300

    working on adding a new pupil

--- src/app/modules/pupil/components/create-pupil/create-pupil.component.ts
+++ src/app/modules/pupil/components/create-pupil/create-pupil.component.ts
@@ -1,12 +1,17 @@
 import { Component, Inject } from '@angular/core';
 
 import { PupilApiInterface } from './../../interfaces/services/pupil-api.interface';
+import { PupilInterface } from './../../interfaces/models/pupil.interface';
+
 
 @Component({
     selector: 'create-pupil',
     templateUrl: '/app/modules/pupil/components/create-pupil/create-pupil.component.html'
 })
 export class CreatePupilComponent {
+
+	pupil: PupilInterface;
+
     constructor(
         @Inject('PupilApiInterface') private pupilApiInterface: PupilApiInterface
     ) {
