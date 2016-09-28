fb9951dc21dc9d788eae58cbace276fb26d89e74
Sharykhin <siarhei.sharykhin@gmail.com>
2016-09-28 18:51:32 +0300

    statted workin on animation

--- src/app/modules/about/about.module.ts
+++ src/app/modules/about/about.module.ts
@@ -3,11 +3,12 @@ import { CommonModule }   from '@angular/common';
 
 import { routing } from './about.routing';
 import { AboutComponent }  from './components/about/about.component';
+import { AdsComponent } from './components/ads/ads.component';
 import { CanAlwaysActivateGuard } from './guards/can-always-activate.guard';
 
 @NgModule({
     imports:      [ routing, CommonModule ],
-    declarations: [ AboutComponent ],
+    declarations: [ AboutComponent, AdsComponent ],
     bootstrap:    [],
     providers: [ CanAlwaysActivateGuard ],
 })
--- src/app/modules/about/components/about/about.component.ts
+++ src/app/modules/about/components/about/about.component.ts
@@ -7,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+    isVisible : boolean = true;
+
     constructor(
     ) {
         console.log('AboutComponent: constructor');
