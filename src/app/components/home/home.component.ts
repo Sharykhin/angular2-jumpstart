55026f4c20e0104a71732dc24b19918c3728ff12
Sharykhin <siarhei.sharykhin@gmail.com>
2016-11-14 12:50:03 +0300

    added moduleId to HomeComponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 
+declare var module: {
+   id: string;
+}
+
 @Component({
     selector: 'home',
-    templateUrl: 'app/components/home/home.component.html',
-    styleUrls: ['app/components/home/home.component.css']
+    moduleId: module.id,
+    templateUrl: 'home.component.html',
+    styleUrls: ['home.component.css']
 })
 export class HomeComponent implements OnInit  {
 
